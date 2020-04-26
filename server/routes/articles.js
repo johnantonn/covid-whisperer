7d65d63e570c7e2746d6fc75ced412042104b27d
Ioannis Antoniadis <johneegr@gmail.com>
2020-04-26 04:50:55 +0300

    Added router handlers for elasticsearch #9

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -12,11 +12,51 @@ const client = new Client({
   } 
 })
 
+// Get all articles
 router.get("/", async (req, res) => {
   try {
-    const results = await client.cat.indices();
-    console.log(results);
+    const results = await client.search({
+      index: process.env.ELASTICSEARCH_INDEX_NAME,
+    });
     return res.status(200).json({ok: true, results});
+  } catch (error) {
+    return res.status(500).json({ ok: false, message: error.message });
+  }
+});
+
+// Get a specific article by id
+router.get("/:ARTICLE_ID/", async (req, res) => {
+  try {
+    const results = await client.get({
+      id: req.params.ARTICLE_ID,
+      index: process.env.ELASTICSEARCH_INDEX_NAME,
+    });
+    return res.status(200).json({ok: true, results});
+  } catch (error) {
+    return res.status(500).json({ ok: false, message: error.message });
+  }
+});
+
+// Store an article
+router.post("/", async (req, res) => {
+  try {
+    await client.index({
+      index: process.env.ELASTICSEARCH_INDEX_NAME,
+      body: req.body
+    })
+  } catch (error) {
+    return res.status(500).json({ ok: false, message: error.message });
+  }
+});
+
+// Update an article
+router.post("/:ARTICLE_ID/", async (req, res) => {
+  try {
+    await client.index({
+      index: process.env.ELASTICSEARCH_INDEX_NAME,
+      id: req.params.ARTICLE_ID,
+      body: req.body
+    })
   } catch (error) {
     return res.status(500).json({ ok: false, message: error.message });
   }
